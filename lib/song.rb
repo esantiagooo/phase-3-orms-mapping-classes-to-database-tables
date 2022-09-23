c11838fcb53019c829a176a6eebddc81eb5d7017
Eric <eric.yanez50@qmail.cuny.edu>
2022-09-23 13:39:57 -0400

    commit tree 4eff4ab0b21331224ca5080fcc0ce9f640a17941

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,43 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :name, :album, :id
 
-  def initialize(name:, album:)
+  def initialize(name:, album:, id: nil)
+    @id = id
     @name = name
     @album = album
   end
+  # creates a new song instances
 
-end
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+      DB[:conn].execute(sql)
+  end
+  # creates a SQL table with column names that match the initialize attributes
+
+  def save 
+    sql = <<-SQL
+    INSERT INTO songs (name, album)
+    VALUES (?,?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.album)
+    # creates a new row in the table for song instances and saves attributes into database
+
+    self.id = DB[:conn].execute('SELECT last_insert_rowid() FROM songs')[0][0]
+    # grabs the song's ID from the database and saves it to ruby instance
+
+    self
+  end 
+
+  def self.create(name:, album:)
+    song = Song.new(name: name, album: album)
+    song.save
+  end
+  end
+
